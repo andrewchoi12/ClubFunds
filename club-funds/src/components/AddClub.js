2e7a4d6c8716e615563cafc446b59706420a4deb
Benjamin Lee <41604522+benjaminjlee10@users.noreply.github.com>
2023-11-29 17:00:16 -0500

    added addclub page with style
    Going to figure out the rewire the routing to this page later, probably with react router

---
+++ club-funds/src/components/AddClub.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import css from "../styles/AddClub.module.css";
+
+export default function AddClub() {
+  return (
+    <main class="AddClub">
+      <header>Add a New Club</header>
+      <div class="Boxes">
+        <div class="Box">
+          <label for="clubName">Club Name:</label>
+          <input
+            type="text"
+            id="clubName"
+            name="clubName"
+            placeholder="Enter Club Name"
+          />
+        </div>
+
+        <div class="Box">
+          <label for="clubDescription">Club Description:</label>
+          <textarea
+            id="clubDescription"
+            name="clubDescription"
+            placeholder="Enter Club Description"
+          ></textarea>
+        </div>
+
+        <div class="Box">
+          <label for="tags">Tags:</label>
+          <input type="text" id="tags" name="tags" placeholder="Enter Tags" />
+        </div>
+
+        <button type="submit" class="SubmitButton">Submit</button>
+      </div>
+    </main>
+  );
+}
