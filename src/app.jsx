b1b02608e5dde1bab7ed6da832bc7fca1a32dfec
Andrew Choi <choibah@bc.edu>
2023-11-29 16:24:37 -0500

    Added React Stuff

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "./components/Home.jsx";
+
+import "./styles/styles.css";
+import Seo from './components/seo.jsx';
+
+export default function App() {
+  return (
+      <Home />
+  );
+}
